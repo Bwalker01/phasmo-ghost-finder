45887aae160193c2238aca3c49027315d391f12f
Ben Walker <benjaminjwalker2910@gmail.com>
2022-11-24 12:49:02 +0000

    table formatting

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <div className="App">
         <Headering updatePage={this.updatePage} />
-        {page}
+        <div className="content">{page}</div>
       </div>
     );
   }
